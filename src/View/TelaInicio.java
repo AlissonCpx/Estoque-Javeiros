fc132cadf923f47e5a81dd84cfe20e7053f29e70
Alisson Andrade <alissondeandrade1@gmail.com>
2019-12-02 23:31:04 -0300

    Tela Vendas pronta

--- src/View/TelaInicio.java
+++ src/View/TelaInicio.java
@@ -188,9 +188,7 @@ public class TelaInicio extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton6ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-        // TODO add your handling code here:
-        //new TelaVendas().setVisible(true);
-        //dispose();
+    new TelaVenda().setVisible(true);
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
